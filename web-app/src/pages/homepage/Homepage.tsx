364d6c81d853fc282dc885eb59734d96586e0365
Jacobsmi <Jacobsm84@gmail.com>
2022-02-15 19:18:51 -0500

    Initial commit

---
+++ web-app/src/pages/homepage/Homepage.tsx
@@ -0,0 +1,9 @@
+const Homepage = () =>{
+    return(
+        <div className="h-screen w-screen bg-home-img bg-cover">
+            
+        </div>
+    )
+}
+
+export default Homepage;
---
+++ web-app/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'home-img': "url('./imgs/field.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
